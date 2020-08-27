793d6fc6e06730b5eae684624163ffee47bdabb2
brennen <bmar566@gmail.com>
2020-08-27 11:33:29 -0500

    finish intro

---
+++ UdemyHelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace UdemyHelloWorld
+{
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "Ok");
+        }
+    }
+}
+ 
